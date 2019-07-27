7dc9577f55df9d1b4d7902288966d0d9529c264c
devdelvo <kevinbdelvo@gmail.com>
2019-07-27 12:05:29 -0400

    replaced lifecycle hooks with useEffect

--- src/App.js
+++ src/App.js
@@ -1,51 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-// const App = () => {
-//   const [count, setCount] = useState(0); // sets count to 0 and has a function setCount for changing the state.
+const App = () => {
+  const [count, setCount] = useState(0); // sets count to 0 and has a function setCount for changing the state.
 
-//   const increment = () => {
-//     setCount(count + 1)
+  useEffect(() => {
+    document.title = `Clicked ${count} times`
+  })
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+  return (
+    <div>
+      <h2>Counter App</h2>
+      <h1>Count: {count}</h1>
+      <button onClick={increment} style={ {height: '50px', width: '50px'} }> + </button>
+    </div>
+  )
+}
+
+// class App extends React.Component {
+//   state = {
+//     count: 0,
 //   }
 
-//   return (
-//     <div>
-//       <h2>Counter App</h2>
-//       <h1>Count: {count}</h1>
-//       <button onClick={increment} style={ {height: '50px', width: '50px'} }> + </button>
-//     </div>
-//   )
-// }
+//   increment = () => {
+//     const { count } = this.state;
+//     this.setState({
+//       count: count + 1
+//     })
+//   }
 
-class App extends React.Component {
-  state = {
-    count: 0,
-  }
+//   componentDidMount() {
+//      document.title = `Clicked ${this.state.count} times`
+//   }
 
-  increment = () => {
-    const { count } = this.state;
-    this.setState({
-      count: count + 1
-    })
-  }
+//   componentDidUpdate() {
+//     document.title = `Clicked ${this.state.count} times`
+//  }
 
-  componentDidMount() {
-     document.title = `Clicked ${this.state.count} times`
-  }
-
-  componentDidUpdate() {
-    document.title = `Clicked ${this.state.count} times`
- }
-
-  render() {
-    return (
-      <div>
-        <h2>Counter App</h2>
-        <h1>Count: {this.state.count}</h1>
-        <button onClick={this.increment} style={ {height: '50px', width: '50px'} }> + </button>
-      </div>
+//   render() {
+//     return (
+//       <div>
+//         <h2>Counter App</h2>
+//         <h1>Count: {this.state.count}</h1>
+//         <button onClick={this.increment} style={ {height: '50px', width: '50px'} }> + </button>
+//       </div>
       
-    );
-  }
-}
+//     );
+//   }
+// }
 
 export default App;
