574fb3b7ff2a1b6583a3242af12efff8d2654da6
devdelvo <kevinbdelvo@gmail.com>
2019-07-27 11:03:36 -0400

    created class component for counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
-function App() {
-  return (
-    <h2>Counter App</h2>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  }
+
+  increment = () => {
+    const { count } = this.state;
+    this.setState({
+      count: count + 1
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Counter App</h2>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.increment} style={ {height: '50px', width: '50px'} }> + </button>
+      </div>
+      
+    );
+  }
 }
 
 export default App;
