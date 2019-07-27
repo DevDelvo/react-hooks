71fff2d63d745602661d14bb7bc708315bd324fd
devdelvo <kevinbdelvo@gmail.com>
2019-07-27 12:45:47 -0400

    added loading

--- src/App.js
+++ src/App.js
@@ -2,23 +2,40 @@ import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [news, setNews] = useState([]);
+  const[searchQuery, setSearchQuery] = useState("react");
+  const[url, setUrl] = useState("https://hn.algolia.com/api/v1/search?query=react");
+  const[loading, setLoading] = useState(false);
 
   const fetchNews = () => {
-    fetch(`https://hn.algolia.com/api/v1/search?query=react`)
+    setLoading(true);
+    fetch(url)
     .then(res => res.json())
-    .then(data => setNews(data.hits))
+    .then(data => (setNews(data.hits), setLoading(false)))
     .catch(err => console.log(err));
   }
 
-  // useEffect(() => {
-  //   fetchNews();
-  // })
+  useEffect(() => {
+    fetchNews();
+  }, [url]); // 2nd argument - this function only runs when url is changed
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUrl(`https://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+  }
 
   return(
     <div>
       <h2>NEWS</h2>
+      {loading ? <h2>loading...</h2>: "" }
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={searchQuery} onChange={handleChange}/>
+        <button>Search</button>
+      </form>
       {news.map((n, i) => {
-        console.log(n);
         return <p key={i}>{n.title}</p>
       })}
     </div>
